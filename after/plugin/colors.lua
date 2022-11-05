bef3b58d40a59bb7cf74c2d3b8e64230d7696be9
Shawn Matyasovskzy <shawnmuzick@gmail.com>
2022-11-05 16:14:51 -0400

    color update

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,24 +4,16 @@ vim.g.material_style = "darker"
 vim.opt.background = 'dark'
 require('material').setup(
 {
-
 	contrast = {
-		sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+		sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
 		floating_windows = false, -- Enable contrast for floating windows
-		line_numbers = false, -- Enable contrast background for line numbers
-		sign_column = false, -- Enable contrast background for the sign column
 		cursor_line = false, -- Enable darker background for the cursor line
 		non_current_windows = false, -- Enable darker background for non-current windows
-		popup_menu = false, -- Enable lighter background for the popup menu
 	},
-
-	italics = {
-		comments = true, -- Enable italic comments
-		keywords = false, -- Enable italic keywords
-		functions = false, -- Enable italic functions
-		strings = false, -- Enable italic strings
-		variables = false -- Enable italic variables
-	},
+		-- Give comments style such as bold, italic, underline etc.
+	    styles = { 
+        comments = {  italic = true }
+    },
 }
 )
 vim.cmd 'colorscheme material'
