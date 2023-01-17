c8585859c936dba7c76b500b017a372c7d154c1b
Shawn Matyasovskzy <shawnmuzick@gmail.com>
2023-01-17 17:13:24 -0500

    packer bootstrapping

--- lua/shawn/packer.lua
+++ lua/shawn/packer.lua
@@ -1,6 +1,19 @@
 --PLUGINS
 --
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+--
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -61,5 +74,8 @@ return require('packer').startup(function()
 	use {"akinsho/toggleterm.nvim", tag = 'v2.*', config = function()
 		require("toggleterm").setup()
 	end}
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
 
