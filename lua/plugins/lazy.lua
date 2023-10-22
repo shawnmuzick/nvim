b99079e134d1eb4341fcbdb23de4fa7404717fe1
Shawn Matyasovskzy <shawnmuzick@gmail.com>
2023-10-21 21:27:14 -0400

    new plugins

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -11,41 +11,40 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 plugins ={
-	'wbthomason/packer.nvim',
-	'hiphish/rainbow-delimiters.nvim',
-	'folke/trouble.nvim',
+	--Git
+	'lewis6991/gitsigns.nvim',
+	'tpope/vim-fugitive',
+
 	--lsp autocomplete stuff
-	'VonHeikemen/lsp-zero.nvim',
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	'neovim/nvim-lspconfig',
 	'williamboman/mason.nvim',
 	'williamboman/mason-lspconfig.nvim',
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'L3MON4D3/LuaSnip'},
+	'folke/trouble.nvim',
 	'hrsh7th/nvim-cmp',
 	'hrsh7th/cmp-buffer',
 	'hrsh7th/cmp-path',
-	'saadparwaiz1/cmp_luasnip',
 	'hrsh7th/cmp-nvim-lsp',
 	'hrsh7th/cmp-nvim-lua',
-
-	-- Snippets
+	'saadparwaiz1/cmp_luasnip',
 	'L3MON4D3/LuaSnip',
 	'rafamadriz/friendly-snippets',
 	{
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate'
 	},
+
 	-- fuzzy find
 	"nvim-lua/plenary.nvim",
 	'nvim-telescope/telescope.nvim',
-	-- Git
-	'tpope/vim-fugitive',
-	-- Theme
+
+	-- UI and Theme
 	'marko-cerovac/material.nvim',
-	-- UI
 	'kyazdani42/nvim-web-devicons',
 	'nvim-lualine/lualine.nvim',
 	'akinsho/bufferline.nvim',
-	'kyazdani42/nvim-tree.lua'
+	'kyazdani42/nvim-tree.lua',
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	'hiphish/rainbow-delimiters.nvim',
 }
 require("lazy").setup(plugins, opts)
